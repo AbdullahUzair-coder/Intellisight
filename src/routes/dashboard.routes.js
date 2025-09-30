40e600ee81603f84e5dd6b5019798d0e597a4de1
Abdullah Uzair <abdullahozair000@gmail.com>
2025-09-30 23:43:09 +0500

    Teacher and students display

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,14 +1,22 @@
 import express from "express";
-import { dashboardpage,dashboardData, studentsPage, teachersPage, zonesPage, logsPage } from "../controllers/dashboard.controller.js";
+import { dashboardpage,dashboardData, studentsPage, teachersPage, zonesPage, logsPage ,getStudentLogs,getStudentLogsById,getTeacherLogsById,getTeacherLogs} from "../controllers/dashboard.controller.js";
 import { authMiddleware } from "../bussiness/middlewares/authentication.js";
 
 const router = express.Router();
 
 router.get("/api/dashboard", authMiddleware, dashboardData);
 router.get("/dashboard", authMiddleware,dashboardpage);
+
 router.get("/students", authMiddleware, studentsPage);
+router.get("/api/student-logs", authMiddleware, getStudentLogs);
+router.get("/api/student-logs/:id", authMiddleware, getStudentLogsById);
+
 router.get("/teachers", authMiddleware, teachersPage);
+router.get("/api/teacher-logs", authMiddleware, getTeacherLogs);
+router.get("/api/teacher-logs/:id", authMiddleware, getTeacherLogsById);
+
 router.get("/zones", authMiddleware, zonesPage);
 router.get("/logs", authMiddleware, logsPage);
+
 
 export default router;
